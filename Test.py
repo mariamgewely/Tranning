e34a4f5a4e6585d83f1ac294ca257c1245e74e37
mariammubarakIT <mubzmubarak623@gmail.com>
2025-02-23 22:06:21 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,9 @@
 # على كل فرد في الفريق كتابة اسمه والرقم التعريفي بالترتيب كما هو موضح دون حذف الاكواد السابقة
 name1 = "Maryam Eid"
-id_number1 = 2320603  
+id_number1 = 2320603 
 print("الاسم:", name1)
 print("ID:", id_number1)
+name3 = "mariam mohamed mubarak"
+id_name3 =2320604
+print("الاسم:" name3)
+print("ID:", id_number3)
